8d5b37fa6a71ee0290bf31b28ee3afde56c8d7f9
Dennis Luo <dennis.luo1@gmail.com>
2017-09-10 21:40:36 -0400

    update seeds file

--- to_do_app/db/seeds.rb
+++ to_do_app/db/seeds.rb
@@ -5,27 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Todolist.create(
-[
-  {
-    title: "Grocery Shopping",
-    description: "Eggs, milk, orange juice, veggies, chicken, bread, tuna, peanut butter"
-  },
-  {
-    title: "Do Laundry",
-    description: "Get laundry done by Saturday"
-  },
-  {
-    title: "Group Project",
-    description: "Complete chatApp using Mongo and host it"
-  },
-  {
-    title: "Go For a Run",
-    description: "Go for weekly run. Need to get some exercise in before Monday"
-  },
-  {
-    title: "Cook Dinner",
-    description: "Make a good supper to end the weekend with family"
-  }
-]
-)
+Todolist.create!(title: "Grocery Shopping", description: "Eggs, milk, orange juice, veggies, chicken, bread, tuna, peanut butter")
+Todolist.create!(title: "Do Laundry", description: "Get laundry done by Saturday")
+Todolist.create!(title: "Group Project", description: "Complete chatApp using Mongo and host it")
+Todolist.create!(title: "Go For a Run", description: "Go for weekly run. Need to get some exercise in before Monday")
+Todolist.create!(title: "Cook Dinner", description: "Make a good supper to end the weekend with family")
+
+puts "You have #{Todolist.count} things to do"
