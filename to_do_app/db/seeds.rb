6734c666ba4aeee2f7317474dd30ee5432e32c17
Dennis Luo <dennis.luo1@gmail.com>
2017-09-10 22:32:08 -0400

    change seeds file data

--- to_do_app/db/seeds.rb
+++ to_do_app/db/seeds.rb
@@ -5,10 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Todolist.create!(title: "Grocery Shopping", description: "Eggs, milk, orange juice, veggies, chicken, bread, tuna, peanut butter")
-Todolist.create!(title: "Do Laundry", description: "Get laundry done by Saturday")
-Todolist.create!(title: "Group Project", description: "Complete chatApp using Mongo and host it")
-Todolist.create!(title: "Go For a Run", description: "Go for weekly run. Need to get some exercise in before Monday")
-Todolist.create!(title: "Cook Dinner", description: "Make a good supper to end the weekend with family")
 
-puts "You have #{Todolist.count} things to do"
+morning = Todo.create!(name: 'Morning', time_of_day: 'morning')
+afternoon = Todo.create!(name: 'Afternoon', time_of_day: 'afternoon')
+evening = Todo.create!(name: 'Evening', time_of_day: 'evening')
+
+# To Do Items
+
+Todolist.create!(title: "Talk to Career Coach", description: "Talk to career coach regarding career advice and options", todo: morning)
+Todolist.create!(title: "Attend Class", description: "Attend class from 9am to 5pm", todo: morning)
+Todolist.create!(title: "Grab Lunch With Friend", description: "Grab lunch with friends at Shake Shack", todo: afternoon)
+Todolist.create!(title: "Finish Up Project With Team", description: "Make final touches to our Github repo before submitting", todo: afternoon)
+Todolist.create!(title: "Complete Homework Assignment", description: "Stay after class to finish up homework", todo: afternoon)
+Todolist.create!(title: "Meet Up With Friend And Her Puppy", description: "Meet up with a friend from college and give treat to her awesome puppy", todo: evening)
+Todolist.create!(title: "Head To The Gym", description: "Head to the gym and get workout in for the day", todo: evening)
+Todolist.create!(title: "Make Dinner", description: "Make an awesome dinner", todo: evening)
+
