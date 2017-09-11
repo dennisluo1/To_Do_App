4d4a6bb6bf9d887d46e3fad8b94bbd7df43d4c0e
Dennis Luo <dennis.luo1@gmail.com>
2017-09-10 21:32:10 -0400

    update seeds file

--- to_do_app/db/seeds.rb
+++ to_do_app/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todolist.create(
+[
+  {
+    title: "Grocery Shopping",
+    description: "Eggs, milk, orange juice, veggies, chicken, bread, tuna, peanut butter"
+  },
+  {
+    title: "Do Laundry",
+    description: "Get laundry done by Saturday"
+  },
+  {
+    title: "Group Project",
+    description: "Complete chatApp using Mongo and host it"
+  },
+  {
+    title: "Go For a Run",
+    description: "Go for weekly run. Need to get some exercise in before Monday"
+  },
+  {
+    title: "Cook Dinner",
+    description: "Make a good supper to end the weekend with family"
+  }
+]
+)
