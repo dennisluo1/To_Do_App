f0ede85d64e299b66e9aac6e2404982a8cb455eb
Dennis Luo <dennis.luo1@gmail.com>
2017-09-12 09:46:31 -0400

    set up crud, need to fix bugs

--- to_do_app/app/controllers/lists_controller.rb
+++ to_do_app/app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+    
     def index
         @todo = Todo.find(params[:todo_id])
         @lists = @todo.lists
@@ -8,4 +9,50 @@ class ListsController < ApplicationController
         @list = List.find(params[:id])
     end
     
+    def new
+        @list = List.new
+    end
+    
+    def create
+        @list = List.new(list_params)
+        if @list.valid?
+            @author.save
+            redirect_to @list
+        else
+            flash[:list] = @list.errors.messages
+            redirect_back fallback_location: new_list_path
+        end
+    end
+    
+    def edit 
+        @list = List.find(params[:id])
+    end
+
+    private
+    
+    def update
+        if @list.update(list_params)
+            redirect_to @list
+        else
+            redirect_back fallback_location: edit_list_path(@list)
+        end
+    end
+    
+    def destroy
+        if @list.destroy
+            redirect_to lists_path
+        else
+            redirect_back fallback_location: @list
+        end
+    end
+
+    private
+    
+    def list_params
+        params.require(:list).permit(:title, :description)
+    end
+    
+    def set_list
+        @list = List.find(params[:id])
+    end
 end
